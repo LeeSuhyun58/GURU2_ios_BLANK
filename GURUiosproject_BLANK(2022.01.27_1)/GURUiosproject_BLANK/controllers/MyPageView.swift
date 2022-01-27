4f59aadcdd2d50013a90976e56063f1411a54947
SOO <68263533+LeeSuhyun58@users.noreply.github.com>
2022-01-27 09:14:46 +0900

    Add files via upload

---
+++ GURUiosproject_BLANK(2022.01.27_1)/GURUiosproject_BLANK/controllers/MyPageView.swift
@@ -0,0 +1,16 @@
+//
+//  MyPageView.swift
+//  GURUiosproject_BLANK
+//
+//  Created by Jake on 2022/01/26.
+//
+
+import UIKit
+
+class MyPageView:UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ GURUiosproject_BLANK(2022.01.27_1)/GURUiosproject_BLANK/controllers/SettingView.swift
@@ -0,0 +1,14 @@
+//
+//  SettingView.swift
+//  GURUiosproject_BLANK
+//
+//  Created by Jake on 2022/01/26.
+//
+
+import UIKit
+
+class SettingView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
